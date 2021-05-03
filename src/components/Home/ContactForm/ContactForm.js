c61330ea6dc42fc647168bf15adc3b4c5bab72b6
shakilibn <mdsh540@gmail.com>
2021-05-03 20:59:02 +0600

    lottie added in contact

--- src/components/Home/ContactForm/ContactForm.js
+++ src/components/Home/ContactForm/ContactForm.js
@@ -1,10 +1,22 @@
 import { Button } from 'react-bootstrap';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './ContactForm.css'
 import emailjs from 'emailjs-com';
 import ParticleBackground from '../../ParticleBackground/ParticleBackground';
+import lottie from 'lottie-web';
 
 const ContactForm = () => {
+    const container = useRef(null);
+
+    useEffect(()=>{
+        lottie.loadAnimation({
+            container: container.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../../images/animation/contact.json')
+        })
+    },[])
 
     const sendMessage = (e) => {
         e.preventDefault();
@@ -23,18 +35,27 @@ const ContactForm = () => {
     return (
         <section className="main-container p-3">
             <ParticleBackground />
-            < h2 className="text-center pt-5 brand-text"> Contact me</h2 >
-            <h5 className="text-center mb-4 text-muted">If you wanna get in touch, talk to me about a project collaboration or just say hi</h5>
 
-            <div className="d-flex justify-content-center">
-                <form className="form-area" action="" onSubmit={sendMessage}>
-                    <input style={{transform:'scale(1)'}} type="text" name="name" className="form-control mb-3 input-style" placeholder="your name" />
-                    <input style={{transform:'scale(1)'}} type="text" name="email" className="form-control mb-3 input-style" placeholder="your email Address" />
-                    <textarea className="form-control mb-3 input-style" name="message" id="" rows="5" placeholder="write your message"></textarea>
-                    <div className="d-flex justify-content-end">
-                        <input style={{zIndex:1}} className="intro-btn" type="submit" value="Send message" />
+            <div className="row">
+                <div className="col-md-6">
+
+                    < h2 className="text-center pt-5 brand-text"> Contact me</h2 >
+                    <h5 className="text-center mb-4 text-muted pb-4">If you wanna get in touch, talk to me about a project collaboration or just say hi</h5>
+
+                    <div className="d-flex justify-content-center">
+                        <form className="form-area" action="" onSubmit={sendMessage}>
+                            <input style={{ transform: 'scale(1)' }} type="text" name="name" className="form-control mb-3 input-style" placeholder="your name" />
+                            <input style={{ transform: 'scale(1)' }} type="text" name="email" className="form-control mb-3 input-style" placeholder="your email Address" />
+                            <textarea className="form-control mb-3 input-style" name="message" id="" rows="5" placeholder="write your message"></textarea>
+                            <div className="d-flex justify-content-end">
+                                <input style={{ zIndex: 1 }} className="intro-btn" type="submit" value="Send message" />
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
+                <div className="col-md-6">
+                    <div className="container" ref={container}></div>
+                </div>
             </div>
 
         </section >
